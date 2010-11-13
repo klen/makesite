7057204eab8bdb6bbee6ad5e20c8574bd6c07e7b
Kirill Klenov <horneds@gmail.com>
2010-11-13 14:29:46 +0300

    Repair setup

--- sitegen/__init__.py
+++ sitegen/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from sitegen.main import deploy, remove, list_projects
-
 __version__ = VERSION = "0.07"
 __author__ = AUTHOR = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "public domain"
