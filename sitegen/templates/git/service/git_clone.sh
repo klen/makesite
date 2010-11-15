e88816baa1bf6fec60eb452bd2a9a8348f6879c9
Kirill Klenov <horneds@gmail.com>
2010-11-15 12:34:46 +0300

    Update readme

--- sitegen/templates/git/service/git_clone.sh
+++ sitegen/templates/git/service/git_clone.sh
@@ -1,5 +1,10 @@
-git clone {{repo}} {{deploy_dir}}/source
-cd {{deploy_dir}}/source
+GIT_PROJECT_TEMP_DIR=/tmp/{{ project_name }}{{ branch }}
+GIT_PROJECT_DIR={{deploy_dir}}/source
+
+git clone {{repo}} $GIT_PROJECT_TEMP_DIR
+cd $GIT_PROJECT_TEMP_DIR
 git push origin origin:refs/heads/{{branch}}
 git fetch origin
 git checkout --track origin/{{branch}}
+
+sudo mv $GIT_PROJECT_TEMP_DIR $GIT_PROJECT_DIR
