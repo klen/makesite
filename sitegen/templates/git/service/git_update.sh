21c79c4c2d26da3e0209ffdfe942f82ceaeeb3fc
Kirill Klenov <horneds@gmail.com>
2010-11-15 16:23:40 +0300

    Version 0.1.1

--- sitegen/templates/git/service/git_update.sh
+++ sitegen/templates/git/service/git_update.sh
@@ -3,3 +3,6 @@ GIT_PROJECT_DIR=$!deploy_dir!$/source
 
 cp -r $GIT_PROJECT_DIR $GIT_PROJECT_TEMP_DIR
 cd $GIT_PROJECT_TEMP_DIR && git reset --hard HEAD && git pull
+sudo rm -rf $GIT_PROJECT_DIR
+sudo mv $GIT_PROJECT_TEMP_DIR $GIT_PROJECT_DIR
+sudo chown -R $!user!$:$!group!$ $GIT_PROJECT_DIR
--- sitegen/templates/uwsgi/service/wsgi_update.sh
+++ sitegen/templates/uwsgi/service/wsgi_update.sh
@@ -1,3 +1,4 @@
 SUPERVISOR_PROGRAMM_NAME=$!branch!$.$!project_name!$
 
 sudo supervisorctl restart $SUPERVISOR_PROGRAMM_NAME
+
