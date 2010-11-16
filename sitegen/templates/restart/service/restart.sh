4d920299bcac9c311b16eadb76f61faabe506e52
Kirill Klenov <horneds@gmail.com>
2010-11-16 18:35:17 +0300

    Repair supervisor restart

--- sitegen/templates/restart/service/restart.sh
+++ sitegen/templates/restart/service/restart.sh
@@ -1,12 +1,14 @@
-echo "Restart services"
+SUPERVISORD=/usr/local/bin/supervisord
+SUPERVISORCTL=/usr/local/bin/supervisorctl
 
 if [ -f /etc/init.d/nginx ]; then
+    echo "Restart nginx"
     sudo /etc/init.d/nginx stop
     sudo /etc/init.d/nginx start
 fi
 
 if [ -f /etc/init.d/supervisor ]; then
-    sudo /etc/init.d/supervisor stop
-    sleep 2
-    sudo /etc/init.d/supervisor start
+    echo "Restart supervisor"
+    $SUPERVISORCTL shutdown
+    $SUPERVISORD
 fi
