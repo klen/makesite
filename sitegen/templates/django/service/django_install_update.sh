ae34f47efb1bc3e117fac815ab83761455324efc
Kirill Klenov <horneds@gmail.com>
2010-11-17 19:28:01 +0300

    fix

--- sitegen/templates/django/service/django_install_update.sh
+++ sitegen/templates/django/service/django_install_update.sh
@@ -1,10 +1,10 @@
-BASERUN={{ deploy_dir }}/deploy/base_run.sh
+BASERUN={{ deploy_dir }}/service/base_run.sh
 DJANGO_SETTINGS={{ django_settings }}
 
 if [ -f $BASERUN ]; then
     echo "Run django migration."
-    sh $BASERUN manage.py migrate settings=$DJANGO_SETTINGS
+    sh $BASERUN manage.py migrate --settings=$DJANGO_SETTINGS
 
     echo "Run django collect static files."
-    sh $BASERUN manage.py collectstaticfiles settings=$DJANGO_SETTINGS
+    sh $BASERUN manage.py collectstaticfiles --settings=$DJANGO_SETTINGS
 fi
---
+++ sitegen/templates/restart/service/restart_update.sh
@@ -0,0 +1,11 @@
+BRANCH={{ branch }}
+PROJECT={{ project }}
+SUPERVISOR_PROGRAMM_NAME=$PROJECT.$BRANCH
+SUPERVISOR_CONFPATH={{ supervisorconfpath }}
+
+if ! which supervisorctl >/dev/null; then echo "  * I require supervisorctl but it's not installed."; exit 0; fi
+
+if [ -f $SUPERVISOR_CONFPATH ]; then
+    echo "Restart supervisor service: $SUPERVISOR_PROGRAMM_NAME."
+    sudo supervisorctl restart $SUPERVISOR_PROGRAMM_NAME
+fi
