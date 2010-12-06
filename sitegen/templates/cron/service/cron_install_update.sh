e5685c71a45541d7dd24edcbc13991f582530351
Kirill Klenov <horneds@gmail.com>
2010-12-06 16:51:11 +0300

    Fix cron

--- sitegen/templates/cron/service/cron_install_update.sh
+++ sitegen/templates/cron/service/cron_install_update.sh
@@ -1,14 +1,9 @@
 CRON_CRONFILE={{ cron_conffile }}
 CRON_PROJECTFILE={{ cron_projectfile }}
 CRON_PARSESCRIPT={{ deploy_dir }}/service/cron_parse.py
-CRON_GENERATEFILE={{ deploy_dir }}/deploy/{{ project }}.{{ branch }}.cron
 
 which cron 1>/dev/null || { echo "ERROR: * I require cron but it's not installed."; exit 0; }
 
 if [ -f $CRON_PROJECTFILE ]; then
     sudo python $CRON_PARSESCRIPT
-    if [ -f $CRON_GENERATEFILE ]; then
-        echo "  * Create link to generate project crontabfile:"$CRON_GENERATEFILE
-        sudo ln -sf $CRON_GENERATEFILE $CRON_CRONFILE
-    fi
 fi
--- sitegen/templates/cron/service/cron_remove.sh
+++ sitegen/templates/cron/service/cron_remove.sh
@@ -1,6 +1,6 @@
-CRON_CRONFILE={{ cron_conffile }}
+CRON_OUTPUTFILE={{ cron_outputfile }}
 
-if [ -f $CRON_CRONFILE ]; then
-    echo "  * Remove cron file '$CRON_CRONFILE'."
-    sudo rm -rf $CRON_CRONFILE
+if [ -f $CRON_OUTPUTFILE ]; then
+    echo "  * Remove cron file '$CRON_OUTPUTFILE'."
+    sudo rm -rf $CRON_OUTPUTFILE
 fi
