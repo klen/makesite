cad30af2b25c9958da449a207a61758f1908ba56
Kirill Klenov <horneds@gmail.com>
2010-12-08 16:41:06 +0300

    fix zeta

--- sitegen/templates/zeta/service/zeta_install_update.sh
+++ sitegen/templates/zeta/service/zeta_install_update.sh
@@ -1,4 +1,4 @@
-ZETA_STATICDIR = {{ project_staticdir }}
+ZETA_STATICDIR={{ project_staticdir }}
 
 # Check zeta-library
 which zeta 1>/dev/null || { echo "ERROR: * I require zeta but it's not installed."; exit 0; }
