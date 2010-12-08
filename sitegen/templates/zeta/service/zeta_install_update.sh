f3703e5e361cf43dc771642ab36596d77f805526
Kirill Klenov <horneds@gmail.com>
2010-12-08 19:50:03 +0300

    zeta to sudo

--- sitegen/templates/zeta/service/zeta_install_update.sh
+++ sitegen/templates/zeta/service/zeta_install_update.sh
@@ -3,4 +3,4 @@ ZETA_STATICDIR={{ project_staticdir }}
 # Check zeta-library
 which zeta 1>/dev/null || { echo "ERROR: * I require zeta but it's not installed."; exit 0; }
 
-zeta $ZETA_STATICDIR
+sudo zeta $ZETA_STATICDIR
