d4a25d974f16c93613f956da87c5913cd0e5a74b
Denis Moskalets <denya.msk@gmail.com>
2011-01-22 16:35:25 +0300

    Add --ignore-installed to pip install

--- makesite/templates/virtualenv/service/virtualenv_install.sh
+++ makesite/templates/virtualenv/service/virtualenv_install.sh
@@ -96,6 +96,6 @@ _memcache_to_ve
 
 if [ -f $PIP_PROJECTFILE ]; then
     echo "  * Update virtualenv requirements '$PIP_PROJECTFILE'."
-    sudo pip -E $VIRTUALENVDIR install -r $PIP_PROJECTFILE
+    sudo pip -E $VIRTUALENVDIR install -I -r $PIP_PROJECTFILE
     sudo rm -rf $VIRTUALENVDIR/.reqsum && sudo sh -c "md5sum $PIP_PROJECTFILE > $VIRTUALENVDIR/.reqsum"
 fi
