99776c2748f07faab8a5190ab68dc6098973f26e
Kirill Klenov <horneds@gmail.com>
2011-02-02 17:08:14 +0300

    Disable intercept redirects in dev mode.

--- makesite/modules/django/settings/dev.py
+++ makesite/modules/django/settings/dev.py
@@ -11,3 +11,4 @@ if DEBUG:
     INTERNAL_IPS += tuple('192.168.1.%s' % x for x in range( 99, 111 ))
     MIDDLEWARE_CLASSES += ( 'debug_toolbar.middleware.DebugToolbarMiddleware', )
     INSTALLED_APPS += ( 'debug_toolbar', )
+    DEBUG_TOOLBAR_CONFIG = dict( INTERCEPT_REDIRECTS = False)
