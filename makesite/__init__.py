3ba54a980cc90815f6e1019b674057571a7161b4
Kirill Klenov <horneds@gmail.com>
2011-10-04 17:03:15 +0400

    Another git fix.

--- makesite/__init__.py
+++ makesite/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-version_info = (0, 7, 31)
+version_info = (0, 7, 32)
 
 __version__ = version = '.'.join(map(str, version_info))
 __project__ = PROJECT = __name__
--- makesite/templates/src-git/service/src-git_update.sh
+++ makesite/templates/src-git/service/src-git_update.sh
@@ -12,8 +12,7 @@ SITE_GROUP={{ site_group }}
 PROJECT_SOURCEDIR={{ project_sourcedir }}
 
 cmd_or_die "sudo chown -R $USER:$USER $PROJECT_SOURCEDIR"
-cmd "cd $PROJECT_SOURCEDIR"
-cmd "git reset --hard HEAD"
-cmd "git clean -df"
-cmd "git pull origin $BRANCH"
+cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git reset --hard HEAD"
+cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git clean -df"
+cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git pull origin $BRANCH"
 cmd_or_die "sudo chown -R $SITE_USER:$SITE_GROUP $PROJECT_SOURCEDIR"
