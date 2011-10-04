b5a10f844121e4f0a27990a17985d54ebd3a40d5
Kirill Klenov <horneds@gmail.com>
2011-10-04 17:25:10 +0400

    Some fucking fix.

--- makesite/templates/src-git/service/src-git_update.sh
+++ makesite/templates/src-git/service/src-git_update.sh
@@ -14,5 +14,5 @@ PROJECT_SOURCEDIR={{ project_sourcedir }}
 cmd_or_die "sudo chown -R $USER:$USER $PROJECT_SOURCEDIR"
 cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git reset --hard HEAD"
 cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git clean -df"
-cmd "git --work-tree $PROJECT_SOURCEDIR --git-dir $PROJECT_SOURCEDIR/.git pull origin $BRANCH"
+cmd "cd $PROJECT_SOURCEDIR && git pull origin $BRANCH"
 cmd_or_die "sudo chown -R $SITE_USER:$SITE_GROUP $PROJECT_SOURCEDIR"
