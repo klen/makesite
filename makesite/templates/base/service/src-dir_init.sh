e3164a1c1119935f5c80d355746908e6093386c2
Kirill Klenov <horneds@gmail.com>
2011-10-27 03:06:01 +0400

    Fix src-dir

--- makesite/templates/base/service/src-dir_init.sh
+++ makesite/templates/base/service/src-dir_init.sh
@@ -8,9 +8,13 @@ source $PROJECT_SERVICEDIR/.bsfl
 SRC={{ src }}
 DEPLOY_DIR={{ deploy_dir }}
 PROJECT_SOURCEDIR={{ project_sourcedir }}
+SRC_STATIC_DIR=$PROJECT_SOURCEDIR/static
 
 # Change rights
 cmd_or_die "sudo chown -R $USER:$USER $DEPLOY_DIR"
 
 # Copy source directory to project source directory
 cmd_or_die "cp -rf $SRC $PROJECT_SOURCEDIR"
+if [ -d $SRC_STATIC_DIR ]; then
+    cmd "sudo cp -r $SRC_STATIC_DIR/* $PROJECT_STATICDIR"
+fi
