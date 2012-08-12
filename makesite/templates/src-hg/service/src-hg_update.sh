45f532f23170ddf7f85a646a2b1142a147e740f9
Kirill Klenov <horneds@gmail.com>
2012-08-12 16:05:23 +0400

    Fix src_user

--- makesite/templates/src-hg/service/src-hg_update.sh
+++ makesite/templates/src-hg/service/src-hg_update.sh
@@ -3,5 +3,5 @@
 . $(dirname $0)/utils.sh
 
 cmd_or_die "sudo chown -R $USER:$USER $SOURCE_DIR"
-cmd_or_die "cd $SOURCE_DIR && hg update -C && hg pull"
+cmd_or_die "sudo -u $SRC_USER cd $SOURCE_DIR && hg update -C && hg pull"
 cmd_or_die "sudo chown -R $SITE_USER:$SITE_GROUP $SOURCE_DIR"
